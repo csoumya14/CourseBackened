f390212846338afbd55afa2069c929327d04a853
Soumya Chalakkal <soumyachalakkal@gmail.com>
2024-01-15 20:44:32 +0100

    added dist from front-end

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
+
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -89,6 +91,6 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
